3b6e6524b717abf917b86cf089ff4f7ae5ecc6f1
marcusvgarcia-maker <marcus.v.garcia@iesb.edu.br>
2026-08-21 11:10:50 -0300

    finalizando a aula02 (incompleto)

---
+++ aulas/aula02/grafo_matriz.h
@@ -0,0 +1,15 @@
+#ifndef GRAFO_MATRIZ_H
+#define GRAFO_MATRIZ_H
+
+#define LIMITE 10
+
+typedef struct {
+    int adjacencia[LIMITE][LIMITE];
+    int num_vertices;
+} GrafoMatriz;
+
+void incializar(GrafoMatriz *grafo, int numero);
+void inserir_aresta(GrafoMatriz *grafo, int u, int v);
+void exibir_matriz(GrafoMatriz *grafo);
+
+#endif
---
+++ aulas/aula02/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "grafo_matriz.h"
+
+int main() {
+    int numero_vertices = 8;
+
+    GrafoMatriz grafo;
+    
+    incializar(&grafo, numero_vertices);
+
+    inserir_aresta(&grafo, 0, 1);
+    inserir_aresta(&grafo, 0, 2);
+    inserir_aresta(&grafo, 0, 3);
+    inserir_aresta(&grafo, 1, 4);
+    inserir_aresta(&grafo, 1, 5);
+    inserir_aresta(&grafo, 2, 3);
+    inserir_aresta(&grafo, 2, 6);
+    inserir_aresta(&grafo, 3, 6);
+    inserir_aresta(&grafo, 7, 4);
+    inserir_aresta(&grafo, 7, 5);
+    inserir_aresta(&grafo, 7, 6);
+
+    printf("Matriz de Adjacencia - grafo não orientado:\n");
+
+    inserir_aresta(&grafo, 2, 3);
+    inserir_aresta(&grafo, 2, 6);
+    inserir_aresta(&grafo, 3, 6);
+    inserir_aresta(&grafo, 7, 4);
+    inserir_aresta(&grafo, 7, 5);
+    inserir_aresta(&grafo, 7, 6);
+
+    printf("Matriz de Adjacencia - grafo não")
+    exibir_matriz((&grafo));
+
+    incializar(&grafo, 3);
+}
